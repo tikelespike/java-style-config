b744defc5ec06429797f25cb384bb7defa97b63b
Timo Weberruß <timo.weberruss@student.kit.edu>
2024-09-15 15:33:42 +0200

    Clean up printing

--- style-pre-commit-hook.sh
+++ style-pre-commit-hook.sh
@@ -7,11 +7,10 @@
 # checkstyle-compliant after applying the autoformatter, the user can accept to
 # automatically apply the formatter.
 
-echo "Codestyle pre-commit check enabled."
-
 # Configuration
 NO_FORMATTING_WHEN_CHECKSTYLE_OK=false # Set this to true to skip the (slow) IntelliJ autoformatter when there are no checkstyle violations on the changed files
 USE_AUTOFORMATTER=true # Set this to false to only run checkstyle and abort directly if violations are found (even if they could be fixed by running the autoformatter)
+USE_CHECKSTYLE=false # Set this to false to disable checkstyle and only apply the autoformatter
 ALLOW_IGNORE_CHECKSTYLE=true # Set this to true to allow committing even if this results in checking in checkstyle violations (warning will be shown)
 
 STYLE_REPO=$(dirname $0)
@@ -29,10 +28,26 @@ USER_OPTION_CANCEL="Cancel Commit"
 USER_OPTION_COMMIT_NO_FORMATTING_WITH_VIOLATION="Commit Without Formatting (VIOLATES CHECKSTYLE!)"
 USER_OPTION_COMMIT_NO_FORMATTING_NO_VIOLATION="Commit Without Formatting"
 
+LOG_PREFIX="[STYLE]"
+
+print_info() {
+    echo "$LOG_PREFIX" "$@"
+}
+
+print_info_inline() {
+    echo -n "$LOG_PREFIX" "$@"
+}
 
 print_error() {
-    echo -e "\033[31m""$@" "\033[0m" >&2
+    echo -e "$LOG_PREFIX" "\033[31m""$@""\033[0m" >&2
 }
+
+print_info "Java codestyle pre-commit hook enabled."
+
+if [ "$USE_CHECKSTYLE" = false ] && [ "$USE_AUTOFORMATTER" = false ]; then
+    print_info "No checks enabled."
+    exit 0
+fi
 
 # Check if required tools are available
 if [ "$USE_AUTOFORMATTER" = true ] && [ ! command -v $AUTOFORMATTER &> /dev/null ]; then
@@ -49,7 +64,7 @@ fi
 FILES=$(git diff --cached --name-only --diff-filter=ACM | grep '\.java$')
 
 if [ -z "$FILES" ]; then
-    echo "No Java files changed."
+    print_info "No Java files changed."
     exit 0
 fi
 
@@ -60,28 +75,28 @@ if [ "$USE_AUTOFORMATTER" = false ]; then
     [ -e "$CHECKSTYLE_LOG_ORIGINAL" ] && rm "$CHECKSTYLE_LOG_ORIGINAL"
     for FILE in $FILES; do
         ORIGINAL_FILE=$(git rev-parse --show-toplevel)/$FILE
-        echo "File: $ORIGINAL_FILE" >> "$CHECKSTYLE_LOG_ORIGINAL"
+        print_info "File: $ORIGINAL_FILE" >> "$CHECKSTYLE_LOG_ORIGINAL"
         checkstyle -c "$CHECKSTYLE_CONFIG" "$ORIGINAL_FILE" &>> "$CHECKSTYLE_LOG_ORIGINAL"
         if [ $? -ne 0 ]; then
             CHECKSTYLE_VIOLATION_FILES+=("$FILE")
             CHECKSTYLE_VIOLATIONS_ON_ORIGINAL_FILES=true
         fi
-        echo "" >> "$CHECKSTYLE_LOG_FORMATTED"
+        print_info "" >> "$CHECKSTYLE_LOG_FORMATTED"
     done
     
     if [ "$CHECKSTYLE_VIOLATIONS_ON_ORIGINAL_FILES" = false ]; then
         exit 0
     fi
     
-    echo ""
+    print_info ""
     print_error "Checkstyle violations found in the following file(s):"
     for FILE in "${CHECKSTYLE_VIOLATION_FILES[@]}"; do
         print_error "    $FILE"
     done
-    echo ""
+    print_info ""
     
     if [ "$ALLOW_IGNORE_CHECKSTYLE" = true ]; then
-        echo -n "There are checkstyle violations. Commit anyway (y/n)? "
+        print_info_inline "There are checkstyle violations. Commit anyway (y/n)? "
         read COMMIT_WITH_VIOLATIONS < /dev/tty
 
         if [ "$COMMIT_WITH_VIOLATIONS" != "${COMMIT_WITH_VIOLATIONS#[Yy]}" ]; then
@@ -96,19 +111,19 @@ fi
 # If corresponding setting enabled, skip autoformatter when no checkstyle violations are found
 CHECKSTYLE_VIOLATIONS_ON_ORIGINAL_FILES=unknown
 if [ "$NO_FORMATTING_WHEN_CHECKSTYLE_OK" = true ]; then
-    echo "Running checkstyle..."
+    print_info "Running checkstyle..."
     CHECKSTYLE_VIOLATIONS_ON_ORIGINAL_FILES=false
     for FILE in $FILES; do
         ORIGINAL_FILE=$(git rev-parse --show-toplevel)/$FILE
         checkstyle -c "$CHECKSTYLE_CONFIG" "$ORIGINAL_FILE" &> /dev/null
         if [ $? -ne 0 ]; then
-            echo "Checkstyle violation found in file $FILE."
+            print_info "Checkstyle violation found in file $FILE."
             CHECKSTYLE_VIOLATIONS_ON_ORIGINAL_FILES=true
             break # We can skip checking other files because we will now definitly run the autoformatter
         fi
     done
     if [ "$CHECKSTYLE_VIOLATIONS_ON_ORIGINAL_FILES" = false ]; then
-        echo "Checkstyle ok, skipping autoformatter."
+        print_info "Checkstyle ok, skipping autoformatter."
         exit 0
     fi
 fi
@@ -129,7 +144,7 @@ for FILE in $FILES; do
 done
 
 # Run IntelliJ formatter on temp copies
-echo "Running autoformatter, this can take a few seconds..."
+print_info "Running autoformatter, this can take a few seconds..."
 $AUTOFORMATTER -s $AUTOFORMATTER_CONFIG "${TEMP_COPIES[@]}" &> /dev/null
 
 # Early reject if there are checkstyle issues even after applying the autoformatter
@@ -138,21 +153,21 @@ CHECKSTYLE_VIOLATIONS_ON_FORMATTED_FILES=false
 [ -e "$CHECKSTYLE_LOG_FORMATTED" ] && rm "$CHECKSTYLE_LOG_FORMATTED"
 for FILE in $FILES; do
     TEMP_FILE="$WORKING_COPY_DIR/$FILE"
-    echo "File: $TEMP_FILE" >> "$CHECKSTYLE_LOG_FORMATTED"
+    print_info "File: $TEMP_FILE" >> "$CHECKSTYLE_LOG_FORMATTED"
     checkstyle -c "$CHECKSTYLE_CONFIG" "$TEMP_FILE" &>> "$CHECKSTYLE_LOG_FORMATTED"
     if [ $? -ne 0 ]; then
         CHECKSTYLE_VIOLATION_FILES+=("$FILE")
         CHECKSTYLE_VIOLATIONS_ON_FORMATTED_FILES=true
     fi
-    echo "" >> "$CHECKSTYLE_LOG_FORMATTED"
+    print_info "" >> "$CHECKSTYLE_LOG_FORMATTED"
 done
 if [ "$CHECKSTYLE_VIOLATIONS_ON_FORMATTED_FILES" = true ]; then
-    echo ""
+    print_info ""
     print_error "Checkstyle violations found (with formatter applied) in the following file(s):"
     for FILE in "${CHECKSTYLE_VIOLATION_FILES[@]}"; do
         print_error "    $FILE"
     done
-    echo ""
+    print_info ""
     if [ "$ALLOW_IGNORE_CHECKSTYLE" = false ]; then
         print_error "Commit cancelled. Please fix the checkstyle issues."
         exit 1
@@ -174,12 +189,12 @@ done
 # Autoformatter already applied
 if [ ${#FILES_TO_FORMAT[@]} -eq 0 ]; then
     if [ "$CHECKSTYLE_VIOLATIONS_ON_FORMATTED_FILES" = false ]; then
-        echo "Files already correctly formatted."
+        print_info "Files already correctly formatted."
         exit 0
     fi
 
-    echo ""
-    echo -n "Nothing to do for the autoformatter, but there are still checkstyle violations. Commit anyway (y/n)? "
+    print_info ""
+    print_info_inline "Nothing to do for the autoformatter, but there are still checkstyle violations. Commit anyway (y/n)? "
     read COMMIT_WITH_VIOLATIONS < /dev/tty
 
     if [ "$COMMIT_WITH_VIOLATIONS" != "${COMMIT_WITH_VIOLATIONS#[Yy]}" ]; then
@@ -197,17 +212,17 @@ if [ "$CHECKSTYLE_VIOLATIONS_ON_ORIGINAL_FILES" = unknown ]; then
         CHECKSTYLE_VIOLATIONS_ON_ORIGINAL_FILES=true
     else
         [ -e "$CHECKSTYLE_LOG_ORIGINAL" ] && rm "$CHECKSTYLE_LOG_ORIGINAL"
-        echo "Running checkstyle on original files..."
+        print_info "Running checkstyle on original files..."
         CHECKSTYLE_VIOLATIONS_ON_ORIGINAL_FILES=false
         for FILE in $FILES; do
             ORIGINAL_FILE=$(git rev-parse --show-toplevel)/$FILE
-            echo "File: $ORIGINAL_FILE" >> "$CHECKSTYLE_LOG_ORIGINAL"
+            print_info "File: $ORIGINAL_FILE" >> "$CHECKSTYLE_LOG_ORIGINAL"
             checkstyle -c "$CHECKSTYLE_CONFIG" "$ORIGINAL_FILE" &>> "$CHECKSTYLE_LOG_ORIGINAL"
             if [ $? -ne 0 ]; then
                 CHECKSTYLE_VIOLATIONS_ON_ORIGINAL_FILES=true
                 break # We can skip checking other files because the user will now definitly have to accept the autoformatter
             fi
-            echo "" >> "$CHECKSTYLE_LOG_ORIGINAL"
+            print_info "" >> "$CHECKSTYLE_LOG_ORIGINAL"
         done
     fi
 fi
@@ -228,12 +243,12 @@ if [ "$CHECKSTYLE_VIOLATIONS_ON_ORIGINAL_FILES" = true ] && [ "$ALLOW_IGNORE_CHE
 fi
 QUERY_USER=true
 while [ "$QUERY_USER" = true ]; do
-    echo ""
-    echo "There are new/changed files that are not yet autoformatted correctly."
+    print_info ""
+    print_info "There are new/changed files that are not yet autoformatted correctly."
     if [ "$CHECKSTYLE_VIOLATIONS_ON_ORIGINAL_FILES" = true ] && [ "$CHECKSTYLE_VIOLATIONS_ON_FORMATTED_FILES" = false ]; then
-        echo "There are also checkstyle violations, but they can be fixed automatically."
+        print_info "There are also checkstyle violations, but they can be fixed automatically."
     elif [ "$CHECKSTYLE_VIOLATIONS_ON_ORIGINAL_FILES" = true ] && [ "$CHECKSTYLE_VIOLATIONS_ON_FORMATTED_FILES" = true ]; then
-        echo "There are also checkstyle violations that cannot be fixed automatically."
+        print_info "There are also checkstyle violations that cannot be fixed automatically."
     fi
     select CHOICE in "${USER_OPTIONS[@]}"; do
         case $CHOICE in
@@ -252,7 +267,7 @@ while [ "$QUERY_USER" = true ]; do
                 if [ ! -d "$ORIGINAL_FILES_DIR" ]; then
                     mkdir "$ORIGINAL_FILES_DIR" # Original files (but new/changed only) without autoformatting to compare
                     for FILE in $FILES; do
-                        echo "File: $FILE"
+                        print_info "File: $FILE"
                         ORIGINAL_FILE=$(git rev-parse --show-toplevel)/$FILE
                         mkdir -p "$(dirname "$ORIGINAL_FILES_DIR/$FILE")"
                         cp "$ORIGINAL_FILE" "$ORIGINAL_FILES_DIR/$FILE"
